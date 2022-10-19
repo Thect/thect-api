fe05685e3016d884e50540763568620d114b1e81
Christiaan Molendijk <christiaan.molendijk@deptagency.com>
2022-10-19 09:59:32 +0200

    Fixed unit test by mocking dependency & added more tests

--- tests/unit/functions/spaces/getMySpaces.test.ts
+++ tests/unit/functions/spaces/getMySpaces.test.ts
@@ -1,5 +1,23 @@
 import * as handler from '../../../../src/functions/spaces/getMySpaces/handler';
 
+jest.mock('../../../../src/services/SpacesService', () => {
+  return {
+    SpacesService: jest.fn().mockImplementation(() => {
+      return {
+        getMySpaces: jest.fn().mockResolvedValue([
+          {
+            id: 'test-1',
+            name: 'test Space',
+            description: 'The space for testing',
+            locales: [],
+            users: [],
+          },
+        ]),
+      };
+    }),
+  };
+});
+
 describe('Spaces', () => {
   describe('getMySpaces', () => {
     describe('handler', () => {
@@ -7,17 +25,39 @@ describe('Spaces', () => {
         jest.resetModules();
       });
 
-      it('Should return 0 results', async () => {
+      it('Should return 200 status code', async () => {
         const event = {
           headers: {
             'x-validated-user': 'test-user-id',
           },
         } as any;
 
-        expect(handler.getMySpaces(event)).resolves.toStrictEqual({
-          body: '{"data":[],"count":0}',
-          statusCode: 200,
-        });
+        const result = await handler.getMySpaces(event);
+        expect(result.statusCode).toBe(200);
+      });
+
+      it('Should return a JSON body', async () => {
+        const event = {
+          headers: {
+            'x-validated-user': 'test-user-id',
+          },
+        } as any;
+
+        const result = await handler.getMySpaces(event);
+        const returnValue = JSON.parse(result.body);
+        expect(returnValue).not.toBeNull();
+      });
+
+      it('Should return 1 in count', async () => {
+        const event = {
+          headers: {
+            'x-validated-user': 'test-user-id',
+          },
+        } as any;
+
+        const result = await handler.getMySpaces(event);
+        const returnValue = JSON.parse(result.body);
+        expect(returnValue.count).toBe(1);
       });
     });
   });
