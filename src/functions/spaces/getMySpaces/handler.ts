a7613c1d7bdf375c14f54fa9b48a4c840b429c3a
Christiaan Molendijk <christiaan.molendijk@deptagency.com>
2022-10-19 10:43:23 +0200

    Added more tests

--- src/functions/spaces/getMySpaces/handler.ts
+++ src/functions/spaces/getMySpaces/handler.ts
@@ -6,9 +6,19 @@ import { SpacesService } from 'src/services/SpacesService';
 export const spaceService = new SpacesService(createContext());
 
 export const getMySpaces = async (event) => {
-  const userId = event.headers['x-validated-user']?.valueOf();
-  const from = Number(event.queryStringParameters?.from);
-  const limit = Number(event.queryStringParameters?.limit);
+  let userId = '';
+  if (event.headers) {
+    userId = event.headers['x-validated-user']?.valueOf();
+  }
+
+  let from = NaN;
+  let limit = NaN;
+  if (event.queryStringParameters) {
+    console.log(JSON.stringify(event.queryStringParameters));
+    from = Number(event.queryStringParameters?.from);
+    limit = Number(event.queryStringParameters?.limit);
+    console.log(JSON.stringify({ userId, from, limit }));
+  }
 
   const result = await spaceService.getMySpaces({ userId, from, limit });
 
--- tests/unit/functions/spaces/getMySpaces.test.ts
+++ tests/unit/functions/spaces/getMySpaces.test.ts
@@ -4,15 +4,17 @@ jest.mock('../../../../src/services/SpacesService', () => {
   return {
     SpacesService: jest.fn().mockImplementation(() => {
       return {
-        getMySpaces: jest.fn().mockResolvedValue([
-          {
-            id: 'test-1',
-            name: 'test Space',
-            description: 'The space for testing',
-            locales: [],
-            users: [],
-          },
-        ]),
+        getMySpaces: jest.fn().mockImplementation(() => {
+          return [
+            {
+              id: 'test-1',
+              name: 'test Space',
+              description: 'The space for testing',
+              locales: [],
+              users: [],
+            },
+          ];
+        }),
       };
     }),
   };
@@ -58,6 +60,48 @@ describe('Spaces', () => {
         const result = await handler.getMySpaces(event);
         const returnValue = JSON.parse(result.body);
         expect(returnValue.count).toBe(1);
+      });
+
+      it('Should call get my spaces with correct user id', async () => {
+        const event = {
+          headers: {
+            'x-validated-user': 'test-user-id',
+          },
+        } as any;
+
+        await handler.getMySpaces(event);
+        const mockedSpacesService = handler.spaceService;
+        const mockedGetMySpaces = mockedSpacesService.getMySpaces as jest.Mock;
+        const values = mockedGetMySpaces.mock.calls[mockedGetMySpaces.mock.calls.length - 1][0];
+        expect(values.userId).toBe('test-user-id');
+      });
+
+      it('Should call get my spaces with correct from', async () => {
+        const event = {
+          queryStringParameters: {
+            from: '99',
+          },
+        } as any;
+
+        await handler.getMySpaces(event);
+        const mockedSpacesService = handler.spaceService;
+        const mockedGetMySpaces = mockedSpacesService.getMySpaces as jest.Mock;
+        const values = mockedGetMySpaces.mock.calls[mockedGetMySpaces.mock.calls.length - 1][0];
+        expect(values.from).toBe(99);
+      });
+
+      it('Should call get my spaces with correct limit', async () => {
+        const event = {
+          queryStringParameters: {
+            limit: '99',
+          },
+        } as any;
+
+        await handler.getMySpaces(event);
+        const mockedSpacesService = handler.spaceService;
+        const mockedGetMySpaces = mockedSpacesService.getMySpaces as jest.Mock;
+        const values = mockedGetMySpaces.mock.calls[mockedGetMySpaces.mock.calls.length - 1][0];
+        expect(values.limit).toBe(99);
       });
     });
   });
